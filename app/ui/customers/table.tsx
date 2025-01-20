86187fcca851545e775e58b3482753333cf0edc1
Andrew Taghi <aataghi13@gmail.com>
2025-01-20 11:47:23 -0600

    made changes up to the nav links

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import Search from '@/app/ui/search';
-import { FormattedCustomersTable } from '@/app/lib/definitions';
+import Image from "next/image";
+
+import Search from "@/app/ui/search";
+import { FormattedCustomersTable } from "@/app/lib/definitions";
+import { lusitana } from "../fonts";
 
 export default async function CustomersTable({
   customers,
