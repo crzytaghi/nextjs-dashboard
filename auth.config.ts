a613319a08b2a8a6cd2df858dc84ef7c209399e1
Andrew Taghi <aataghi13@gmail.com>
2025-01-21 16:43:52 -0600

    completed section 15

---
+++ auth.config.ts
@@ -0,0 +1,22 @@
+import { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  // Add logic to protect your routes, this will prevent users from accessing the dashboard pages unless they are logging in
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
